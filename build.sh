cadc2c8297d28f7b57fc8d5fd09158edc76bc675
Ziwei Huang <huang-ziwei@outlook.com>
2026-03-16 11:49:40 +0100

    fix: codesigning on apple silicon

--- build.sh
+++ build.sh
@@ -30,7 +30,7 @@ fi
 
 /usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier $BUNDLE_ID" "$CONTENTS_DIR/Info.plist" >/dev/null 2>&1 || true
 
-IDENTITY=$(security find-identity -v -p codesigning | head -1 | sed 's/.*"\(.*\)".*/\1/')
+IDENTITY=$(security find-identity -v -p codesigning | grep '"' | head -1 | sed 's/.*"\(.*\)".*/\1/')
 if [ -n "$IDENTITY" ]; then
   codesign --force --sign "$IDENTITY" "$APP_DIR"
 else
